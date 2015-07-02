5a3215eb4d03589b79c4bb7efb14d34a9585dc08
Rohith Madhavan <rohithmadhavan@gmail.com>
2015-07-02 10:35:09 +0530

    Added events and failsafe cpp files

---
+++ APMsubmarine/events.cpp
@@ -0,0 +1,8 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#include "Submarine.h"
+
+void Submarine::update_events(void)
+{
+    ServoRelayEvents.update_events();
+}
